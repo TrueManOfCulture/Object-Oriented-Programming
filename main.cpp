cbd085c177c21fec204ee240507306f9bbd7a8a7
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-11-14 17:10:05 +0000

    test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main(){
+    int i;
     cout<< "Ola"<<endl;
+    cout<<"This should be working now"<<endl;
+    cout <<"Write a random int: "<< endl;
+    cin >> i;
     return 0;
 }
