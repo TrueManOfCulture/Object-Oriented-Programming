dacfc883432c49aa85454746ebbb5547077985fa
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2023-11-27 14:35:52 +0000

    Arrumar problemas

--- include/Maquina.h
+++ include/Maquina.h
@@ -20,15 +20,14 @@ class Maquina
     int posX, posY;
     int TEMPERATURA;
     string TIPO;
-    ESTADO_MAQUINA ESTADO;
-    int POSICAO[2];                                 
+    ESTADO_MAQUINA ESTADO;                             
 
     public:
-Maquina::Maquina(float _prob_ganhar, int _premio, int _posX, int _posY, int pos[2], int _temp, string _tipo, User *U, ESTADO_MAQUINA _est);
-        
-        void ReturnBytes(){ cout << "ID: " << sizeof(ID) << endl << "PROB_GANHAR: " << sizeof(PROB_GANHAR) << endl << "PREMIO: " << sizeof(PREMIO) << endl << "PosX: " << sizeof(posX) << endl << "PosY: " << sizeof(posY) << endl << "Temp: " << sizeof(TEMPERATURA) << endl << "TIPO: " << sizeof(TIPO) << endl << "ESTADO: " << sizeof(ESTADO) << endl << endl; }
-        
+        Maquina(float _prob_ganhar, int _premio, int _posX, int _posY, int _temp, string _tipo, User *U = NULL, ESTADO_MAQUINA _est = OFF);
         virtual ~Maquina();
+
+        void ReturnBytes(){ cout << "ID: " << sizeof(ID) << endl << "PROB_GANHAR: " << sizeof(PROB_GANHAR) << endl << "PREMIO: " << sizeof(PREMIO) << endl << "PosX: " << sizeof(posX) << endl << "PosY: " << sizeof(posY) << endl << "Temp: " << sizeof(TEMPERATURA) << endl << "TIPO: " << sizeof(TIPO) << endl << "ESTADO: " << sizeof(ESTADO) << endl << endl; }
+
         ESTADO_MAQUINA Get_ESTADO(){ return ESTADO; }
         int Get_ID (){ return ID; }
         float Get_PROB_GANHAR(){ return PROB_GANHAR; }
