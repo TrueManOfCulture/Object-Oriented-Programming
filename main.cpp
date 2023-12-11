a6cff2a624688046916fd2a582a97761175e4e2a
Kikoszzz <52199358+Kikoszzz@users.noreply.github.com>
2023-12-11 19:36:12 +0000

    Att Tarefas

--- main.cpp
+++ main.cpp
@@ -137,7 +137,7 @@ int main()
     */
 
    //////////////    TESTES DE JOGADORES MAIS GANHOS    //////////////
-   
+   /*
    cout << "//////////////////////////////////////////" << endl << endl;
 
    list<User *>::iterator userIt = LU.begin();
@@ -180,4 +180,5 @@ int main()
     }
 
     delete Test;
+    */
 }
