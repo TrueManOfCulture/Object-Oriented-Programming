cfc1fd0e45305f56ddafcaba40dd83d793931b89
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-12-12 23:56:51 +0000

    a

--- main.cpp
+++ main.cpp
@@ -199,7 +199,6 @@ int main()
     advance(userIt, 2);
     C.Add(*userIt);
 
-<<<<<<< HEAD
    /*list<User *>::iterator userIt = LU.begin();
    C.Add(*userIt);
 
@@ -217,84 +216,3 @@ int main()
 
    //C.Listar();
 }
-=======
-    userIt = LU.begin();
-    advance(userIt, 3);
-    C.Add(*userIt);
-
-   C.Listar();
-   */
-
-    //////////////    TESTES DE JOGAR MAQUINA    //////////////
-    /*
-    cout << "//////////////////////////////////////////" << endl << endl;
-
-    list<User *>::iterator userIt = LU.begin();
-    C.Add(*userIt);
-
-    map<string, Maquina *> HashMaq;
-    HashMaq = C.Ass_HashMaq();
-
-    map<string, Maquina *>::iterator maqIt = HashMaq.begin();
-    maqIt->second->Set_User(*userIt);
-
-    (*userIt)->Show();
-
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl << endl;
-
-    maqIt->second->Jogar();
-
-    (*userIt)->Show();
-    */
-
-    //////////////    TESTES DE JOGADORES MAIS FREQUENTES    //////////////
-    /*
-    cout << "//////////////////////////////////////////" << endl << endl;
-
-    list<User *>::iterator userIt = LU.begin();
-    C.Add(*userIt);
-
-    userIt = LU.begin();
-    advance(userIt, 1);
-    C.Add(*userIt);
-   
-    userIt = LU.begin();
-    advance(userIt, 2);
-    C.Add(*userIt);
-
-    userIt = LU.begin();
-    advance(userIt, 3);
-    C.Add(*userIt);
-
-    map<string, User *> HashUser;
-    HashUser = C.Ass_HashUser();
-
-    for(map<string, User *>::iterator it = HashUser.begin(); it != HashUser.end(); ++it){
-        int num = AleatorioINT(1, 100);
-        it->second->Set_qntEntradas(num);
-    }
-
-    for(map<string, User *>::iterator it = HashUser.begin(); it != HashUser.end(); ++it){
-        it->second->Show();
-    }
-
-
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl;
-    cout << "//////////////////////////////////////////" << endl << endl;
-
-    list<User *> *Test;
-    Test = C.Jogadores_Mais_Frequentes();
-
-    for(list<User *>::iterator it = Test->begin(); it != Test->end(); ++it){
-        (*it)->Show();
-    }
-
-    delete Test;
-    */
-}
->>>>>>> fd64c97c3a19f040738181737350e5c364c6552c
