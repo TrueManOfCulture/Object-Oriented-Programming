6c6742f65509c6e649951caeaf5120881836ef7a
TrueManOfCulture <88104776+TrueManOfCulture@users.noreply.github.com>
2023-12-13 15:57:02 +0000

    Add files via upload
    Xml writer

---
+++ src/XMLWriter.cpp
@@ -0,0 +1,54 @@
+#include "XMLWriter.h"
+
+XMLWriter::XMLWriter()
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+}
+
+XMLWriter::~XMLWriter()
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+}
+
+string XMLWriter::Tabulacoes(int N)
+{
+    string s = "";
+    for (int i = 0; i < N; i++)
+        s += "---";
+    return s;
+}
+void XMLWriter::WriteStartDocument(string ficheiro)
+{
+    //cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+    NOME_FICHEIRO = ficheiro;
+}
+void XMLWriter::WriteEndDocument()
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+}
+void XMLWriter::WriteElementString(string el, string valor)
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+    ofstream F(NOME_FICHEIRO, ios::app);
+    F << Tabulacoes(TAGS.size()) << "<" << el <<">" << valor << "</" << el << ">" << endl;
+    F.close();
+}
+void XMLWriter::WriteStartElement(string el)
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+    TAGS.push_front(el);
+    ofstream F(NOME_FICHEIRO, ios::app);
+    F << Tabulacoes(TAGS.size()) << "<" << el <<">" << endl;
+    F.close();
+}
+void XMLWriter::WriteEndElement()
+{
+    cout << "Passei em <" << __FUNCTION__ << ">" << endl;
+    //cout << "TAM = " << TAGS.size() << endl;
+    string el = TAGS.front();
+    TAGS.pop_front();
+    //cout << "TAM = " << TAGS.size() << endl;
+    ofstream F(NOME_FICHEIRO, ios::app);
+    F << Tabulacoes(TAGS.size()) << "</" << el <<">" << endl;
+    F.close();
+}
